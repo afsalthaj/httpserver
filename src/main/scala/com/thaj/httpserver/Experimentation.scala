0dad3c2f54437c0fd736d1a310394475e77a739b
Afsal Thaj <afsal.taj06@gmail.com>
2018-02-16 19:01:03 +1100

    Delete unwanted files

--- src/main/scala/com/thaj/httpserver/Experimentation.scala
+++
@@ -1,8 +0,0 @@
-package com.thaj.httpserver
-
-object Experimentation {
-  def main(args: Array[String]): Unit = {
-    val hostName = 8080
-
-  }
-}
--- src/main/scala/com/thaj/httpserver/socket/HttpSocket.scala
+++
@@ -1,9 +0,0 @@
-package com.thaj.httpserver.socket
-
-// All that we need is a server that accepts requests through a socket.
-// All that makes it an httpserver is that it excepts some protocol in the communication - http!
-trait HttpSocket {
-  val portNumber = 8080
-
-
-}
